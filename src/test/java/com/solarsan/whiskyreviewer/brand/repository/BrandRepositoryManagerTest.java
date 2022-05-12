066c236ccaa22ca06bb33b957746ee3ea57ca4f3
Antoni Solarski <antsol1000@gmail.com>
2022-05-12 15:15:41 +0200

    GitHub actions

--- src/test/java/com/solarsan/whiskyreviewer/brand/repository/BrandRepositoryManagerTest.java
+++
@@ -1,7 +0,0 @@
-package com.solarsan.whiskyreviewer.brand.repository;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class BrandRepositoryManagerTest {
-
-}
---
+++ src/test/resources/preliquibase/default.sql
@@ -0,0 +1,1 @@
+create schema if not exists whisky_reviewer_testing;
